8c6f5f46ae9f5e71225241d0126129684bde7bdf
Alex Dawson <arjunsivakumar2@gmail.com>
2023-02-28 23:45:16 +0530

    gradient descent done

--- main.py
+++ main.py
@@ -12,23 +12,20 @@ ys = [1.0, -1.0, -1.0, 1.0]
 
 m = nn.MLP(3, [4, 4, 1])
 
-i = 0
-while True:
-    i+=1
+for i in range(1, 51):
+    #forward pass
     ypred = [m(x) for x in xs]
-
     loss = sum([(yp - y)**2 for y, yp in zip(ys, ypred)])
-    #for p in m.parameters():
-        #print("{:.4f} {:.4f}".format(p.data, p.grad))
-        
-    #print()
+    
+    #backward pass
     for p in m.parameters():
         p.grad = 0.0
-
     loss.backward()
 
+    #gradient descent
     for p in m.parameters():
         p.data += -0.01 * p.grad
 
     print(i,":", loss.data)
-    q = input()
+    
+print([m(x).data for x in xs])
